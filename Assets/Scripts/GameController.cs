fb5ca5e0bec59658a1bba4f403f65fa400d755be
Sasamidaifuku <sasamidaifuku@gmail.com>
2023-09-18 11:30:29 +0900

    Fade機能を実装

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    void Start()
+    //FadeCanvas取得
+    [SerializeField]
+    private Fade fade;
+    //フェード時間(秒)
+    [SerializeField]
+    private float fadeTime;
+    
+    private void Start()
     {
+        //シーン開始時にフェードをかける
+        fade.FadeOut(fadeTime);
         //音楽を流す
         AudioManager.Instance.PlayBGM(AUDIO.BGM_IWASHIRO_AME_NO_HAZAMA);
+    }
+
+    private void Update()
+    {
     }
 }
